9bacfead262c9bde5399c89b0d50eb3dab11f70a
tb-max <a1421760@rmqkr.net>
2021-07-01 20:15:17 +0200

    control led using receiver signal

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,39 @@
 #include <Arduino.h>
 #include <EnableInterrupt.h>
 
-#define BUTTON_PIN 5
-#define DEBOUNCE_DELAY 100 // in ms
+
+#define RC_CH1  2
 
 uint32_t last_interrupt_time = 0;
-uint8_t led_status = 0;
-
+uint16_t value = 1500;
+uint8_t led_value = 0;
 
 void isr_handler() {
-  uint32_t interrupt_time = millis();
-
-  if (interrupt_time - last_interrupt_time > DEBOUNCE_DELAY) {
-    led_status = !led_status;
-    digitalWrite(LED_BUILTIN, led_status);
+  if (digitalRead(RC_CH1) == HIGH) {
+    last_interrupt_time = micros();
+  } else {
+    uint16_t rc_compare = (uint16_t)(micros() - last_interrupt_time);
+    value = rc_compare;
+    
+    led_value = map(value, 1000, 2000, 0, 255); 
+    analogWrite(LED_BUILTIN, led_value);
   }
-
-  last_interrupt_time = interrupt_time;
 }
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT);
+  Serial.begin(57600);
+  pinMode(RC_CH1, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  enableInterrupt(BUTTON_PIN, isr_handler, RISING);
+  enableInterrupt(RC_CH1, isr_handler, CHANGE);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.print("rc value: ");
+  Serial.println(value);
+  Serial.print("led value: ");
+  Serial.println(led_value);
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
 }
 
